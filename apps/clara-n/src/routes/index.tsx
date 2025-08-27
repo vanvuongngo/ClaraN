b565f200df3d703fa50c40c69a87049c54558965
Van Vuong Ngo <20791239+vanvuongngo@users.noreply.github.com>
2025-08-27 17:21:56 +0200

    Reapply "feat(#21): UI i18n for english and german"
    This reverts commit 2c365c2364e1f57798c5a42cecb743adfb74ca28.

--- apps/clara-n/src/routes/index.tsx
+++ apps/clara-n/src/routes/index.tsx
@@ -1,37 +1,6 @@
-import { component$, useVisibleTask$ } from "@builder.io/qwik";
-import type { DocumentHead } from "@builder.io/qwik-city";
-import { useLocalStorage } from "qwik-localstorage";
-import { ClaraAssistant } from "~/components/ClaraAssistant";
+import type { RequestHandler } from "@builder.io/qwik-city";
 
-// from https://github.com/badboysm890/ClaraVerse/blob/main/src/App.tsx
-export default component$(() => {
-  console.debug("ClaraN is a hardfork of the awesome ClaraVerse.");
-  console.debug("Why you'll want to use ClaraN?");
-  console.debug(
-    "Because of the tech stack which is based on modern and lightweight technologies:",
-  );
-  console.debug("Tauri, Rust, Qwik, NodeJS LTS, DaisyUI.");
-
-  const { data: activePage, set: setActivePage } = useLocalStorage<string>(
-    "page",
-    "dashboard",
-  );
-
-  // eslint-disable-next-line qwik/no-use-visible-task
-  useVisibleTask$(() => {
-    console.debug("activePage", activePage.value);
-  });
-
-  return <ClaraAssistant onPageChange={setActivePage} />;
-});
-
-export const head: DocumentHead = {
-  title: "ClaraN - AI Assistant",
-  meta: [
-    {
-      name: "description",
-      content:
-        "ClaraN is a hardfork of the awesome Clara with modern tech stack and mobile first",
-    },
-  ],
+export const onRequest: RequestHandler = ({ redirect, url }) => {
+  // the local en does not matter will be auto detected
+  throw redirect(301, `/en/${url.search}`);
 };
